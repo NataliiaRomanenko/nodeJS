2582944982fbd7ac275b99e3ce27f7be0b6edacf
Nataly Romanenko <nataliia.romanenko@gmail.com>
2019-05-23 18:01:17 +0300

    module util, error

---
+++ user-server/error.js
@@ -0,0 +1,22 @@
+var util = require('util');
+
+var phrases = {
+    "Hello": "Привет",
+    "world": "мир"
+};
+function getPhrases (name) {
+    if(!phrases[name]){
+        throw new Error("Нет такой фразы" + name);
+    }
+    return phrases[name];
+}
+
+function makePage (url) {
+    if(url != 'index.html'){
+        throw new Error ("Нет такой страницы");
+    }
+    return util.format("%s, %s!", getPhrases('Hello'), getPhrases('world'))
+}
+
+var page = makePage('index.html');
+console.log(page);
---
+++ user-server/inspect.js
@@ -0,0 +1,35 @@
+var util = require('util');
+
+// var obj = {
+//     a: 5,
+//     b: 6
+// };
+// obj.self = obj;
+//
+// console.log(util.inspect(obj));
+
+
+// Родитель
+function Animal(name){
+    this.name = name;
+}
+
+Animal.prototype.walk = function(){
+    console.log("Ходит " + this.name);
+};
+
+// Потомок
+function Rabbit(name){
+    this.name = name;
+}
+
+util.inherits(Rabbit, Animal);
+
+Rabbit.prototype.jump = function(){
+    console.log("Прыгает " + this.name);
+};
+
+// Использование
+var rabbit = new Rabbit("наш кролик");
+rabbit.walk();
+rabbit.jump();
